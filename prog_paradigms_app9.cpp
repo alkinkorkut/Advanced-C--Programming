ca4c9021a076bc486fd7a53fa05869ca80bcfeb1
Alkın Korkut <78097693+alkinkorkut@users.noreply.github.com>
2024-02-16 13:00:28 +0300

    Update prog_paradigms_app9.cpp

--- prog_paradigms_app9.cpp
+++ prog_paradigms_app9.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 #include <list>
 
+// generic programming:
+// transform algorithm (map in other languages)
+// inline version with side-effects vs. pure version
+// filter algorithm
+// count_if algorithm
+// accumulate algorithm (reducer in other languages)
+
 using namespace std;
 
 //template<typename Container, typename Func>
@@ -38,8 +45,44 @@ void print(const Container& cont) {
     cout << endl;
 }
 
+auto accumulate(const auto& container, const auto& func, auto init) {
+    auto value = init;
+    for(const auto& item : container) {
+        value = func(value, item);
+    }
+    return value;
+}
 
 int main(int argc, char* argv[]) {
+    auto L = list<float>{1.1f, 2.2f, 3.3f};
+    auto multiplyBy2 = [](float& value) { return value * 2; };
+    //print(L);
+
+    //transform_inline<list<float>, decltype(multiplyBy2)>(L, multiplyBy2);
+    //transform(L, multiplyBy2);
+    auto L_new = transform(L, multiplyBy2);
+    cout << "L: ";
+    print(L);
+    cout << "L_new: ";
+    print(L_new);
+
+    auto L_new_filtered = filter(L_new, [](const auto& item) {
+        return item>4.0f;
+    });
+    cout << "L_new_filtered: ";
+    print(L_new_filtered);
+
+    {
+        auto sum = accumulate(L_new_filtered, [](const auto& value, const auto& item) {
+            return value + item;
+        }, 0.0f);
+        cout << "Sum: " << sum << endl;
+
+        auto product = 1.0f;
+        for(const auto& item : L_new_filtered)
+            product *= item;
+        cout << "Product: " << product << endl;
+    }
 
     return 0;
 }
