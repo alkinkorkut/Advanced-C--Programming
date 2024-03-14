8a541fd9f3175486138da7eae03a7756bc79ae9e
Alkın Korkut <78097693+alkinkorkut@users.noreply.github.com>
2024-03-15 00:26:40 +0300

    Update FuncPointers.cpp

--- FuncPointers.cpp
+++ FuncPointers.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -7,13 +10,25 @@ int doit1(int a, int b) { return a; }
 int doit2(int a, int b) { return a+b; }
 int doit3(int a, int b) { return a-b; }
 
-//typedef int (*TwoInters)(int, int);
-using TwoInters = int (*)(int, int);
+//typedef int (*TwoInters)(int, int); // C type
+using TwoInters = int (*)(int, int); // C++ type
 using OneInter = int (*)(int);
 
 int dosmt1(int a) { return a; }
 int dosmt2(int a) { return a*a; }
 int dosmt3(int a) { return a+1; }
+
+struct Fibonacci {
+    int a = 0;
+    int b = 1;
+
+    int operator() () {
+        auto returningValue = a+b;
+        a = b;
+        b = returningValue;
+        return returningValue;
+    }
+};
 
 template<typename T>
 void for_each(vector<T>& v, OneInter funcptr) {
@@ -30,6 +45,13 @@ void print(const vector<T>& v) {
     }
 }
 
+template<typename T>
+void repeat_it(T funcObject, int times) {
+    for(int i=0; i<times; i++) {
+        cout << funcObject() << endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
     int choice = 1;
     int retval;
@@ -42,7 +64,16 @@ int main(int argc, char* argv[]) {
 
     for_each(V, &dosmt3);
     print(V);
+    cout << "-----" << endl;
 
+    repeat_it(Fibonacci{}, 10);
+    cout << "-----" << endl;
+    repeat_it([a=0, b=1]() mutable  {
+            auto returningValue = a+b;
+            a = b;
+            b = returningValue;
+            return returningValue;
+        },15);
 
 //    if(choice == 1)
 //        func = &doit1;
@@ -65,6 +96,7 @@ int main(int argc, char* argv[]) {
 
 
 //    cout << retval << endl;
+
 
 
 
