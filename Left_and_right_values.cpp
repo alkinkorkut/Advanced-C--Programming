2462ca1ca8dadb600a847219c660d7df10d1fd2b
Alkın Korkut <78097693+alkinkorkut@users.noreply.github.com>
2024-03-16 17:26:07 +0300

    Updated Left_and_right_values.cpp and CMakeLists.txt

--- Left_and_right_values.cpp
+++ Left_and_right_values.cpp
@@ -7,6 +7,17 @@ using namespace std;
 // r-value -> right value
 // chain assignment
 
+void func(int i) { } // l-value, this "i" is within the local scope of the function, this is a variable in the local scope, and l-value.
+// it indicates that this "i" is going to be an integer, it will have a memory address. The value in the memory address is called as "i".
+
+void func2(int&  i) { } // l-value reference, it can only bind to l-values. It can't bind to r-values. The value is an integer. It lives in
+// a flat. It has a memory address. So, give me the memory address of the value. [the flat]
+
+void func3(const int& i) { } // const l-value reference, it can bind to both l-values and r-values. Promise for read-only access.
+
+void func4(const int&& i) { } // r-value reference, it can only bind to r-values. Promise for read-only access.
+// Tell to the compiler, there is a temporary value, and I want to access it. I don't want to modify it. I want to read it.
+
 struct string_ {
     string_(const string&);
     string_(const char*);
@@ -24,6 +35,31 @@ int foo2(const string& str) { // const& situation -> can bind to both l&r values
 }
 
 int main(int argc, char* argv[]) {
+
+    // l-value[function parameter] to l-value[call-side argument], (it clones the value)
+    int i = 1;
+    func(i);
+    // --------
+
+    // l-value[function parameter] to r-value[call-side argument][temporary value], (it clones the value)
+    func(2);
+    // --------
+
+    // l-value reference[function parameter] to l-value[call-side argument], (it doesn't clone the value)
+    int j = 3;
+    func2(j);
+    // --------
+
+    // const l-value reference[function parameter] to l-value and r-value [call-side argument], (it doesn't clone the value)
+    int k = 4;
+    func3(k);
+    func3(5);
+    // --------
+
+    // const r-value reference[function parameter] to r-value[call-side argument][temporary value], (it doesn't clone the value)
+    func4(6);
+    // --------
+
 
     // int a = foo(string("hello world")); // This gives compiler error // bind to r-value
     // Right version of it
